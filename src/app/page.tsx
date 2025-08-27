8feb38c633cdbbe618f02c4ba78d9c03ea801d15
opada <kami18102004@gmail.com>
2025-08-27 23:48:15 +0300

    b

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Navbar from "../../components/Navbar";
+import Showform from "../../components/Showform";
 export default function Home() {
   return (
-    <main className="min-h-screen ">
+    <main className="min-h-screen bg-[url(/sand-photo.jpg)] bg-cover">
       <Navbar />
       <section>
         <div className="p-14 ">
-          
+          <Showform />
         </div>
       </section>
       
