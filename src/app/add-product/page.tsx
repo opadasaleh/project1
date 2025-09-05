9c04a35674ef65b63d67157130a6b13ca3211739
opada <kami18102004@gmail.com>
2025-09-05 20:37:32 +0300

    add bg image and metadata

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Button } from "@/components/ui/button"
 
+export const metadata = {
+    title: 'Add Product - Flowmazon',
+}
 
 export default function AddProduct() {
     return (
-        <div className='min-h-screen flex  items-center justify-center max-w-fit '>
-            <div className="w-full max-w-md p-8 rounded-lg shadow-md border-4  ">
+        <main className='min-h-screen bg-[url(/sand-photo.jpg)] bg-cover pt-5'>
+        <div className='  flex justify-center mt-50'>
+            <div className="w-full max-w-md p-8 rounded-lg shadow-md border-4 bg-white">
                 <h1 className='text-2xl mb-5 font-bold '>Add Product</h1>
                 <form  >
                     <input
                         className='mb-3 w-full border-2 '
                         required
                         name='name'
-                        placeholder='Name'
+                        placeholder=' Name'
                     />
                     <textarea
                         className='mb-3 w-full border-2'
                         name="description"
-                        placeholder='Description'
+                        placeholder=' Description'
                         required
                     />
                     <input
@@ -25,18 +29,19 @@ export default function AddProduct() {
                         required
                         name='imageUrl'
                         type='url'
-                        placeholder='Image URL'
+                        placeholder=' Image URL'
                     />
                     <input
                         className='mb-3 w-full border-2'
                         required
                         type='number'
                         name='price'
-                        placeholder='Price'
+                        placeholder=' Price'
                     />
-                    <Button  variant={'default'} className=''>Add Product</Button>
+                    <Button  variant={'default'} className='bg-amber-400'>Add Product</Button>
                 </form>
             </div>
-        </div>
+            </div>
+        </main>
     )
 }
