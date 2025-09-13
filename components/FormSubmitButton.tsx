154e432d8510c8ce946dbe61fef668572fb6a1e2
opada <kami18102004@gmail.com>
2025-09-13 16:38:58 +0300

    finsh formsubmitbutton

--- components/FormSubmitButton.tsx
+++ components/FormSubmitButton.tsx
@@ -1,20 +1,35 @@
 "use client"
 
-
 import { StringToBoolean } from 'class-variance-authority/types'
 import React, { Children, ComponentProps } from 'react'
 import { Button } from "@/components/ui/button"
 import { Loader2Icon } from "lucide-react"
+import { useFormStatus } from 'react-dom'
+import { Props } from 'next/script'
+import { Spinner } from '@/components/ui/shadcn-io/spinner'
+// import  experimental_useFormStatus as useFormState  from 'react-dom' 
 
 
 type FormSubmitButtonProps = {
-    children: React.ReactNode,
+    // children: React.ReactNode,
     className?: string,
+    
 } & ComponentProps<"button">
-export default function FormSubmitButton({ children, className }: FormSubmitButtonProps) {
+export default function FormSubmitButton({
+    children,
+    className,
+    ...props
+}: FormSubmitButtonProps) {
+    const {pending} = useFormStatus();
     return (
-        <Button size={'default'} variant={'default'} className={'bg-[#00ff66] w-full rounded-xs ${className}'}>{children}
-        {/* <Loader2Icon className='animate-spin'/> */}
-        </Button>
+        <Button size={'default'} variant={'default'} className={'bg-[#2dc168]  w-full rounded-xs ${className}'}
+        disabled={pending}
+        {...props}
+            
+        >
+            {children}
+            {pending && <Loader2Icon className='animate-spin flex justify-end'/> }
+        
+        </Button> 
     )
 }
