2c7937fc955daf72c3a53f6fb61d1d22ab9c5fe8
opada <kami18102004@gmail.com>
2025-09-13 16:39:57 +0300

    fix the issue in layout and it was the className

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
-import Link from 'next/link';
 import '../app/globals.css';
-import { title } from 'process';
+
 export const metadata = {
   title: "Flowmazone",
   description: "We make ayour wallet cry",
-}
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <main className=''>
-
-        {children}
+      <head />
+      <body className='' >
+        <main>
+          {children}
         </main>
       </body>
     </html>
