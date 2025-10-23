c41ef032a590df59a12641971cada1dad5055f76
opada <kami18102004@gmail.com>
2025-10-23 15:18:13 +0300

    upadte ts files

--- src/generated/prisma/index.d.ts
+++ src/generated/prisma/index.d.ts
@@ -171,8 +171,8 @@ export namespace Prisma {
   export import Exact = $Public.Exact
 
   /**
-   * Prisma Client JS version: 6.16.2
-   * Query Engine version: 1c57fdcd7e44b29b9313256c76699e91c3ac3c43
+   * Prisma Client JS version: 6.17.0
+   * Query Engine version: c0aafc03b8ef6cdced8654b9a817999e02457d6a
    */
   export type PrismaVersion = {
     client: string
