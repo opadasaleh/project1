be9ef2ee705339e990905707dfa2959fc66e7fe7
opada <kami18102004@gmail.com>
2025-10-23 15:16:16 +0300

    change the product show to product.map

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -2,33 +2,27 @@ import React from 'react'
 import Navbar from '../../../components/Navbar'
 import { prisma } from '@/lib/db/prisma'
 import CardProduct from '../../../components/CardProduct'
-import { Link } from 'lucide-react'
 
-async function Shop() {
+async function Shop(
+  { params, }: { params: Promise<{ slug: string }> }
+) {
   const products = await prisma.items.findMany({
     orderBy: { id: "desc" }
   })
 
   return (
-    <div>
-      
-      <div className="grid grid-cols-6 gap-1 ">
-        
-          <CardProduct product={products[0]} />
-          <CardProduct product={products[2]} />
-          <CardProduct product={products[0]} />
-          <CardProduct product={products[0]} />
-          <CardProduct product={products[0]} />
-          <CardProduct product={products[0]} />
-          <CardProduct product={products[0]} />
-          <CardProduct product={products[0]} />
-          <CardProduct product={products[0]} />
-          <CardProduct product={products[0]} />
-          <CardProduct product={products[0]} />
-        
-      
+    <div className="min-h-screen bg-[#ffffff]">
+      {/* <Navbar /> */}
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold text-white mb-8 text-center">Shop</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {products.map((product) => (
+            <CardProduct key={product.id} product={product}  />
+          ))}
+        </div>
       </div>
     </div>
   )
 }
 export default Shop
+    
